de7dc10d60c24ff4e21772bbfe898e931dfdf26c
Royalcows9 <Royalcows9@gmail.com>
2017-04-12 19:27:39 -0700

    Added the ability to specialy choose one channel for the cog to run inside
    Also updated readme.md to match the new steps in the setup

--- randomCopyPastaCog.py
+++ randomCopyPastaCog.py
@@ -10,13 +10,22 @@ class RandomCopyPastaCog:
 
     @commands.command()
     async def copypasta(self):
-        """This does stuff!"""
-        reddit = praw.Reddit(client_id='zQ1s57VSHEE3mg',
-                     client_secret="wpiRxlMiwUNB1gxktbqAVG9mvgw",
-                     user_agent='Gets Copypasta from r/copypasta/random')
-        sub = reddit.subreddit('copypasta')
-        post = sub.random()
-        self.bot.say(post.selftext)
-
+        """Put the ID of your spem channel here"""
+        spamChannel = "Put_A_Channel_ID_Here"
+        
+        """This is the actual code"""
+        channel = ctx.message.channel.id
+        if spamChannel = "Put_A_Channel_ID_Here":
+            self.bot.say("Please open the cog's scrypt and change the varriable \'spamChannel\' to the ID of your server's spam channel!")
+        else:
+            if channel = spamChannel:
+                reddit = praw.Reddit(client_id='zQ1s57VSHEE3mg',
+                         client_secret="wpiRxlMiwUNB1gxktbqAVG9mvgw",
+                         user_agent='Gets Copypasta from r/copypasta/random')
+                sub = reddit.subreddit('copypasta')
+                post = sub.random()
+                self.bot.say(post.selftext)
+            else:
+                self.bot.say("Please take this to the spam channel for the sanity of other users. Thanks!!")
 def setup(bot):
     bot.add_cog(RandomCopyPastaCog(bot))
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@ Hey people! Thatns for looking at my bot!!!
 This is a cog for the discord bot Red Management Bot
 
 This cog is pretty simple. It uses PRAW to get a random post from www.reddit.com/r/copypasta and posts it in chat on command
+
+<h3>Setup Guide</h3>
+1) Open a command window in this project's root folder and run "pip install -r requirements.txt"
+2) Change the varriable "spamChannel" from "Put_A_Channel_ID_Here" to your spam channel's channel ID
+3) Load the bot and you're good to go!
